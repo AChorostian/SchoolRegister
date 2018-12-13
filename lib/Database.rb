4700a34ada1ac46cafb44818e74fc5b1d0387a29
dz3lo <andrzejchorostian@outlook.com>
2018-12-13 21:23:33 +0100

    sql create code done

--- lib/Database.rb
+++ lib/Database.rb
@@ -1,5 +1,9 @@
 require 'sqlite3'
 
+# NOTES
+#
+# sqlite available types: BLOB, TEXT, INTEGER, REAL, NUMERIC
+
 class Database
 
     attr_reader :db
@@ -7,19 +11,56 @@ class Database
     def initialize
         begin
             @db = SQLite3::Database.open "database.db"
-            @db.execute "CREATE TABLE IF NOT EXISTS Uczen(uczen_id INTEGER PRIMARY KEY, Name TEXT)"
-            @db.execute "CREATE TABLE IF NOT EXISTS Ocena(ocena_id INTEGER PRIMARY KEY, Name TEXT)"
-            @db.execute "CREATE TABLE IF NOT EXISTS Przedmiot(przedmiot_id INTEGER PRIMARY KEY, Name TEXT)"
-            @db.execute "CREATE TABLE IF NOT EXISTS Uwaga(uwaga_id INTEGER PRIMARY KEY, Name TEXT)"
-            @db.execute "CREATE TABLE IF NOT EXISTS Nauczyciel(nauczyciel_id INTEGER PRIMARY KEY, Name TEXT)"
-            @db.execute "CREATE TABLE IF NOT EXISTS UczenPrzedmiot(uczen_id INTEGER, przedmiot_id INTEGER, Name TEXT)"
+            @db.execute "CREATE TABLE IF NOT EXISTS Uczen(
+                uczen_id INTEGER PRIMARY KEY,
+                imie TEXT,
+                nazwisko TEXT)"
+            @db.execute "CREATE TABLE IF NOT EXISTS Ocena(
+                ocena_id INTEGER PRIMARY KEY,
+                ocena INTEGER,
+                kategoria TEXT,
+                data TEXT,
+                przedmiot_id INTEGER,
+                uczen_id INTEGER)"
+            @db.execute "CREATE TABLE IF NOT EXISTS Przedmiot(
+                przedmiot_id INTEGER PRIMARY KEY,
+                nazwa TEXT,
+                nauczyciel_id INTEGER)"
+            @db.execute "CREATE TABLE IF NOT EXISTS Uwaga(
+                uwaga_id INTEGER PRIMARY KEY,
+                tresc TEXT,
+                data TEXT,
+                uczen_id INTEGER,
+                nauczyciel_id INTEGER)"
+            @db.execute "CREATE TABLE IF NOT EXISTS Nauczyciel(
+                nauczyciel_id INTEGER PRIMARY KEY,
+                imie TEXT,
+                nazwisko TEXT)"
+            @db.execute "CREATE TABLE IF NOT EXISTS UczenPrzedmiot(
+                uczen_id INTEGER,
+                przedmiot_id INTEGER)"
         rescue SQLite3::Exception => e
-            raise
             puts "Exception occurred"
             puts e
         ensure
             @db.close if @db
         end
+    end
+
+    def createuczen uczen
+        puts "xd"
+    end
+    def readuczen uczen_id
+        puts "xd"
+    end
+    def updateuczen uczen
+        puts "xd"
+    end
+    def deleteuczen uczen
+      puts "xd"
+    end
+    def countuczen
+      puts "xd"
     end
 
 end
