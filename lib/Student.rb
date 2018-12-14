059f3d6b87815a33e9be8ddc6ef6c7da0fb61c43
inql <dbinkus96@gmail.com>
2018-12-14 23:40:30 +0100

    remove unnecessary model methods (i know that commit will fail)

--- lib/Student.rb
+++ lib/Student.rb
@@ -15,26 +15,11 @@ class Student
         self
     end
 
-    def setarray(data)
-        @id = data[0]
-        @name = data[1]
-        @surname  = data[2]
-        self
-    end
-
     def gethash
         data = Hash.new
         data[:id] = @id
         data[:name] = @name
         data[:surname] = @surname
-        data
-    end
-
-    def getarray
-        data = Array.new
-        data[0] = @id
-        data[1] = @name
-        data[2] = @surname
         data
     end
 
