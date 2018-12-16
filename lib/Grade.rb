680174bb1922e424aa821b71cb9c9a3db5990b38
Dawid <dbinkus96@gmail.com>
2018-12-16 19:43:39 +0100

    implemented method for validating hash(other fields will be added soon)

--- lib/Grade.rb
+++ lib/Grade.rb
@@ -13,9 +13,27 @@ class Grade
     def validate(data)
       raise ArgumentError, "Wrong type of argument provided!" unless data.is_a? Hash or data.is_a? String or data.is_a? Integer
       if data.is_a? Hash
-        return nil
+        data.each do |key, value|
+          validate_field(key,value)
+        end
       end
     end
+
+    def validate_field(key,value)
+      case key
+      when :id
+        raise ArgumentError, "Wrong type of id field!" unless value.is_a? Integer and value>=0
+      when :grade
+        raise ArgumentError, "Wrong type of grade field!" unless value.is_a? Integer and value>=1 and value<=6
+      when :comment
+        raise ArgumentError, "Wrong type of comment field!" unless value.is_a? String
+      when :date
+        raise ArgumentError, "Wrong type of date field!" unless value.is_a? String and value.match(/\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/)
+      when :studentsubject_id
+        raise ArgumentError, "Wrong type of studentsubject_id field" unless value.is_a? Integer and value >=0
+      else
+        raise ArgumentError, "Wrong key provided, check your input!"
+      end    end
 
     def sethash(data)
         @id = data[:id]
