48518c2087c630e7ee5a11680bd4fc995ce9fb20
Mateusz Małecki <mmateusz314@gmail.com>
2018-12-18 23:12:37 +0100

    Fixed regex

--- lib/Student.rb
+++ lib/Student.rb
@@ -9,7 +9,7 @@ class Student < Sequel::Model(Database.db[:Student])
         validates_presence [:name,:surname], message: 'was not given'
         validates_type String, [:name, :surname], message: 'wrong value type'
         validates_length_range 3..50, [:name,:surname], message: lambda{|s| "should be more than #{s} characters"}
-        validates_format /^([ \u00c0-\u01ffa-zA-Z'\-])+$/, [:name,:surname], message: "contains invalid characters"
+        validates_format /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+(([-][A-ZĄĆĘŁŃÓŚŹŻ])[a-ząćęłńóśźż]+)?$/, [:name,:surname], message: "contains invalid characters"
     end
 
     def self.printlabels
