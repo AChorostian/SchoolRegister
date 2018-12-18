02bcff61f95829382b162afb1718aa268e164f4f
inql <dbinkus96@gmail.com>
2018-12-18 23:48:18 +0100

    add validators for grade

--- lib/Note.rb
+++ lib/Note.rb
@@ -11,8 +11,9 @@ class Note < Sequel::Model(Database.db[:Note])
         validates_type String [:desciption,:date], message: 'wrong value type'
         validates_type Integer [:Teacher_id,:Student_id], message: 'wrong value type'
         validates_length_range 3..500, :desciption, message: lambda{|s| "should be more than #{s} characters"}
-
-        errors.add(:Teacher_id, 'Teacher_id value is lower than 0') if teacher_id <= 0
+        # validates_format /^(?:(?:31(/|-|.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(/|-|.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(/|-|.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(/|-|.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/, :date, message: "contains invalid characters"
+        errors.add(:Teacher_id, 'Teacher_id value is lower than 0') if Teacher_id <= 0
+        errors.add(:Student_id, 'Student_id value is lower than 0') if Student_id <= 0
 
     end
 
