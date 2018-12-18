bd79b424f96cf5dbc9682e9351febd4bb67cc0de
Mateusz Małecki <mmateusz314@gmail.com>
2018-12-19 00:29:22 +0100

    Added tests for Note

--- lib/Note.rb
+++ lib/Note.rb
@@ -7,13 +7,13 @@ class Note < Sequel::Model(Database.db[:Note])
 
     def validate
         super
-        validates_presence [:desciption,:date,:Student_id,:Teacher_id], message: 'was not given'
-        validates_type String [:desciption,:date], message: 'wrong value type'
-        validates_type Integer [:Teacher_id,:Student_id], message: 'wrong value type'
-        validates_length_range 3..500, :desciption, message: lambda{|s| "should be more than #{s} characters"}
+        validates_presence [:description,:date,:Student_id,:Teacher_id], message: 'was not given'
+        validates_type String, [:description,:date], message: 'wrong value type'
+        validates_type Integer, [:Teacher_id,:Student_id], message: 'wrong value type'
+        validates_length_range 3..500, :description, message: lambda{|s| "should be more than #{s} characters"}
 
-        errors.add(:Teacher_id, 'Teacher_id value is lower than 0') if teacher_id <= 0
-
+        errors.add(:Teacher_id, 'Teacher_id value is lower than 0') if self[:Teacher_id] <= 0
+        errors.add(:Student_id, 'Teacher_id value is lower than 0') if self[:Student_id] <= 0
     end
 
     def self.printlabels
--- spec/Note_spec.rb
+++ spec/Note_spec.rb
@@ -26,8 +26,73 @@ end
 
 describe "Checking Note Class functionality" do
 
+  before {
+    @test_student = Student.new
+    @test_student[:name] = "Test"
+    @test_student[:surname] = "Student"
+    @test_student.save
+
+    @test_teacher = Teacher.new
+    @test_teacher[:name] = "Test"
+    @test_teacher[:surname] = "Teacher"
+    @test_teacher.save
+
+    @pre_add_len = Note.dataset.count
+    @note = Note.new
+    @note[:description] = "Description of the note"
+    @note[:date] = "01.01.2018"
+    @note[:Student_id] = @test_student[:id]
+    @note[:Teacher_id] = @test_teacher[:id]
+    @note.save
+
+    @post_add_len = Note.dataset.count
+  }
 
 
+
+  it "Inserting value should increase dataset length" do
+    expect(Note.dataset.count).to eq(@pre_add_len+1)
+  end
+
+  it "Checking if value is inserted correctly" do
+    expect(Note.last).to eq(@note)
+  end
+
+
+  it "Updating value shouldn't increase dataset length" do
+    @updated_note = Note.last
+    @updated_note[:description] = "Testing if Note is ok"
+    @updated_note.save
+    expect(Note.dataset.count).to eq(@post_add_len)
+  end
+
+  it "Checking if value is updated correctly" do
+    @updated_note = Note.last
+    @updated_note[:description] = "Testing if Note is ok"
+    @updated_note.save
+    expect(Note.last).to eq(@updated_note)
+  end
+
+  it "Deleting value should decrease dataset length" do
+    # skip("no mocks implemented")
+    @removed_note = Note.last
+    @removed_note.delete
+    expect(Note.dataset.count).to eq(@post_add_len-1)
+  end
+
+  it "Checking if value is removed correctly" do
+    # skip("no mocks implemented")
+    @removed_note = Note.last
+    @removed_note.delete
+    expect(Note.last).not_to eq(@removed_note)
+  end
+
+
+  after {
+    @test_teacher = nil
+    @test_student = nil
+    @note = nil
+  }
 
 end
 
