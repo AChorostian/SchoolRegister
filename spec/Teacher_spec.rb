939559ebdd0e9ed05b985c72f64944db3496d935
Dawid <dbinkus96@gmail.com>
2018-12-18 08:21:10 +0100

    implement basic teacher tests

--- spec/Teacher_spec.rb
+++ spec/Teacher_spec.rb
@@ -23,3 +23,69 @@ describe "Checking Teacher class initialization" do
   }
 
 end
+
+describe "Checking Teacher class functionality" do
+  before{
+    Database.init
+    @new_teacher = Teacher.new
+    @new_teacher[:name] = "Jan"
+    @new_teacher[:surname] = "Abacki"
+    @new_teacher.save
+    @curr_len = Teacher.dataset.count
+  }
+
+  it "Inserting value should increase dataset length" do
+    expect(Teacher.dataset.count).to eq(@curr_len+1)
+  end
+
+  it "Checking if value is inserted correctly" do
+    expect(Teacher.last).to eq(@new_teacher)
+  end
+
+  it "Updating value shouldn't increase dataset length" do
+    @updated_teacher = @new_teacher
+    @updated_teacher[:surname] = "New-Surname"
+    @updated_teacher.save
+    expect(Teacher.dataset.count).to eq(@curr_len)
+  end
+
+  it "Checking if value is updated correctly" do
+    @updated_teacher = Teacher.last
+    @updated_teacher[:surname] = "New-Surname"
+    @updated_teacher.save
+    expect(Teacher.last).to eq(@updated_teacher)
+  end
+
+  it "Deleting value should decrease dataset length" do
+    skip("no mocks implemented")
+    @removed_teacher = Teacher.last
+    @removed_teacher.delete
+    expect(Teacher.dataset.count).to eq(@curr_len-1)
+  end
+
+  it "Checking if value is removed correctly" do
+    skip("no mocks implemented")
+    @removed_teacher = Teacher.last
+    @removed_teacher.delete
+    expect(Teacher.last).not_to eq(@removed_student)
+  end
+
+  after{
+    @new_teacher = nil
+  }
+end
+
+describe "Checking Student class validation" do
+  before{
+    @new_teacher = Student.new
+  }
+
+  it "lol" do
+    @new_teacher[:name] = nil
+    @new_teacher[:surname] = "surname"
+    expect{@new_teacher.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  after{
+    @new_teacher = nil
+  }
