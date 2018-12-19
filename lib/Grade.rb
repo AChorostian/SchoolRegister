501a67682e284b933392711cd114f89494e5b237
Mateusz Małecki <mmateusz314@gmail.com>
2018-12-19 15:04:46 +0100

    Added validation tests for grade model

--- lib/Grade.rb
+++ lib/Grade.rb
@@ -9,8 +9,10 @@ class Grade < Sequel::Model(Database.db[:Grade])
         validates_includes [1,2,3,4,5,6], :grade, message: 'wrong grade given [1..6] allowed'
         validates_type Integer, :grade, message: 'wrong value type'
         validates_type String, [:comment, :date], message: 'wrong value type'
+
+        validates_format /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/, :date, message: "invalid date format"
         validates_length_range 3..500, :comment, message: lambda{|s| "should be more than #{s} characters"}
-        errors.add(:StudentSubject_id, 'StudentSubject_id value is lower than 0') if self[:StudentSubject_id] <= 0
+        errors.add(:StudentSubject_id, 'StudentSubject_id value is lower than 0') if self[:StudentSubject_id].to_i <= 0
     end
 
     def self.printlabels
--- spec/Grade_spec.rb
+++ spec/Grade_spec.rb
@@ -107,3 +107,109 @@ describe "Checking Grade class functionality" do
 
 
 end
+
+
+describe "Checking Grade class functionality" do
+  before{
+    @test_student = Student.new
+    @test_student[:name] = "Test"
+    @test_student[:surname] = "Student"
+    @test_student.save
+
+    @test_teacher = Teacher.new
+    @test_teacher[:name] = "Test"
+    @test_teacher[:surname] = "Teacher"
+    @test_teacher.save
+
+    @test_subject = Subject.new
+    @test_subject[:name] = "Test"
+    @test_subject[:Teacher_id] = @test_teacher[:id]
+    @test_subject.save
+
+
+    @test_studentSubject = StudentSubject.new
+    @test_studentSubject[:Student_id] = @test_student[:id]
+    @test_studentSubject[:Subject_id] = @test_subject[:id]
+    @test_studentSubject.save
+
+
+    @grade = Grade.new
+    @grade[:grade] = 5
+    @grade[:comment] = "Za projekt z ruby"
+    @grade[:date] = "08.01.2019"
+    @grade[:StudentSubject_id] =@test_studentSubject[:id]
+
+  }
+
+
+  it "Checking error when grade is nil" do
+    @grade[:grade] = nil
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when grade is not integer" do
+    @grade[:grade] = "Dwója"
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when grade is not between 1 and 6" do
+    @grade[:grade] = 7
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when comment is nil" do
+    @grade[:comment] = nil
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when comment is not string" do
+    @grade[:comment] = ["Praca", "Praca"]
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when comment is too short" do
+    @grade[:comment] = "T"
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when date is nil" do
+    @grade[:date] = nil
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when date has invalid format" do
+    @grade[:date] = "2019.01.01"
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when date is not string" do
+    @grade[:date] = 17
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when studentSubject_id is nil" do
+    @grade[:StudentSubject_id] = nil
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when studentSubject_id is not integer" do
+    @grade[:StudentSubject_id] = "test"
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+  it "Checking error when studentSubject_id is negative integer" do
+    @grade[:StudentSubject_id] = -1
+    expect{@grade.save}.to raise_error(Sequel::ValidationFailed)
+  end
+
+
+  after{
+    @test_teacher = nil
+    @test_subject = nil
+    @test_student = nil
+    @grade = nil
+    @test_studentSubject = nil
+  }
+
+
+  end
