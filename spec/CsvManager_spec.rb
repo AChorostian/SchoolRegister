5e4a26439aa359f007b4373681fa2032ee2d56eb
Mateusz Małecki <mmateusz314@gmail.com>
2018-12-19 18:44:13 +0100

    Added file tests for csv

--- spec/CsvManager_spec.rb
+++ spec/CsvManager_spec.rb
@@ -3,15 +3,23 @@ require_relative '../lib/Register.rb'
 describe "Checking import from files" do
 
   before {
-    @db = Sequel.sqlite("database.db")
     Database.init
+    @db = Database.db
+    @test_file = File.new("resources/TestCsv.txt","r")
   }
 
 
-  it "Test importing from csv files" do
-      przed = @db[:Student].all.count
-      CsvManager.importFromCsv("data/students.csv",Student)
-      expect(@db[:Student].all.count).to eq(przed+20)
+  it "Checks importing from csv files" do
+    while (line = @test_file.gets) do
+      path, table, count = line.split(' ')
+      @before = @db[table.to_s.to_sym].all.count
+      CsvManager.importFromCsv(path, table)
+      expect(@db[table.to_s.to_sym].all.count).to eq(@before+count.to_i)
+    end
+  end
+
+  it "Checks exporting to csv files" do
+
   end
 
 
