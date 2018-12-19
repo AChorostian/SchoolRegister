cf0bb86d3819f22a51206a08855a77269817f0eb
Mateusz Małecki <mmateusz314@gmail.com>
2018-12-19 01:01:22 +0100

    Added tests for Grade class

--- lib/Grade.rb
+++ lib/Grade.rb
@@ -7,10 +7,10 @@ class Grade < Sequel::Model(Database.db[:Grade])
     def validate
         validates_presence [:grade, :comment, :date], message: 'was not given'
         validates_includes [1,2,3,4,5,6], :grade, message: 'wrong grade given [1..6] allowed'
-        validates_type Integer :grade, message: 'wrong value type'
-        validates_type String [:comment, :date], message: 'wrong value type'
+        validates_type Integer, :grade, message: 'wrong value type'
+        validates_type String, [:comment, :date], message: 'wrong value type'
         validates_length_range 3..500, :comment, message: lambda{|s| "should be more than #{s} characters"}
-        errors.add(:StudentSubject_id, 'StudentSubject_id value is lower than 0') if StudentSubject_id <= 0
+        errors.add(:StudentSubject_id, 'StudentSubject_id value is lower than 0') if self[:StudentSubject_id] <= 0
     end
 
     def self.printlabels
--- spec/Grade_spec.rb
+++ spec/Grade_spec.rb
@@ -24,17 +24,86 @@ describe "Checking Grade class initialization" do
 
 end
 
-describe "Checking Grade class correctness" do
+describe "Checking Grade class functionality" do
   before{
+    @test_student = Student.new
+    @test_student[:name] = "Test"
+    @test_student[:surname] = "Student"
+    @test_student.save
+
+    @test_teacher = Teacher.new
+    @test_teacher[:name] = "Test"
+    @test_teacher[:surname] = "Teacher"
+    @test_teacher.save
+
+    @test_subject = Subject.new
+    @test_subject[:name] = "Test"
+    @test_subject[:Teacher_id] = @test_teacher[:id]
+    @test_subject.save
+
+
+    @test_studentSubject = StudentSubject.new
+    @test_studentSubject[:Student_id] = @test_student[:id]
+    @test_studentSubject[:Subject_id] = @test_subject[:id]
+    @test_studentSubject.save
+
+    @pre_add_len = Grade.dataset.count
+
     @grade = Grade.new
+    @grade[:grade] = 5
+    @grade[:comment] = "Za projekt z ruby"
+    @grade[:date] = "08.01.2019"
+    @grade[:StudentSubject_id] =@test_studentSubject[:id]
+    @grade.save
+
+    @post_add_len = Subject.dataset.count
   }
-    it "test" do
-      expect(true).to eq(true)
-    end
 
 
-  after{ @grade = nil
+  it "Inserting value should increase dataset length" do
+    expect(Grade.dataset.count).to eq(@pre_add_len+1)
+  end
+
+  it "Checking if value is inserted correctly" do
+    expect(Grade.last).to eq(@grade)
+  end
+
+  it "Updating value shouldn't increase dataset length" do
+    @updated_grade= Grade.last
+    @updated_grade[:grade] = 6
+    @updated_grade.save
+    expect(Grade.dataset.count).to eq(@post_add_len)
+  end
+
+  it "Checking if value is updated correctly" do
+    @updated_grade = Grade.last
+    @updated_grade[:grade] = 6
+    @updated_grade.save
+    expect(Grade.last).to eq(@updated_grade)
+  end
+
+  it "Deleting value should decrease dataset length" do
+    # skip("no mocks implemented")
+    @removed_grade = Grade.last
+    @removed_grade.delete
+    expect(Grade.dataset.count).to eq(@post_add_len-1)
+  end
+
+  it "Checking if value is removed correctly" do
+    # skip("no mocks implemented")
+    @removed_grade = Grade.last
+    @removed_grade.delete
+    expect(Grade.last).not_to eq(@removed_grade)
+  end
+
+  after{
+    @test_teacher = nil
+    @test_subject = nil
+    @test_student = nil
+    @grade = nil
+    @test_studentSubject = nil
   }
+
 
 
 end
