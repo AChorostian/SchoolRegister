26b92f16910f8dccf6c318beebb45695252b803c
inql <dbinkus96@gmail.com>
2018-12-22 19:20:04 +0100

    Implement minitest for StudentSubject model

--- test/StudentSubject_test.rb
+++ test/StudentSubject_test.rb
@@ -17,7 +17,219 @@ describe "Checking StudentSubject class initialization" do
     assert_kind_of(Sequel::Model,@student_subject)
   end
 
+  it "StudentSubject is associated with :StudentSubject table" do
+    assert_equal :StudentSubject, StudentSubject.table_name
+  end
+
   after{
     @student_subject = nil
   }
+end
+describe "Checking StudentSubject Class functionality" do
+
+  before {
+    Database.init
+    @test_student = Student.new
+    @test_student[:name] = "Test"
+    @test_student[:surname] = "Student"
+    @test_student.save
+
+    @test_teacher = Teacher.new
+    @test_teacher[:name] = "Test"
+    @test_teacher[:surname] = "Teacher"
+    @test_teacher.save
+
+    @test_subject = Subject.new
+    @test_subject[:name] = "Testing subject"
+    @test_subject[:Teacher_id] = @test_teacher[:id]
+    @test_subject.save
+
+    @pre_add_len = StudentSubject.dataset.count
+
+    @studentSubject = StudentSubject.new
+    @studentSubject[:Student_id] = @test_student[:id]
+    @studentSubject[:Subject_id] = @test_subject[:id]
+    @studentSubject.save
+
+    @post_add_len = StudentSubject.dataset.count
+  }
+
+  it "Inserting value should increase dataset length" do
+    assert_equal @pre_add_len+1, StudentSubject.dataset.count
+  end
+
+  it "Checking if value is inserted correctly" do
+    assert_equal @studentSubject, StudentSubject.last
+  end
+
+  it "Updating value shouldn't increase dataset length" do
+    @updated_studentSubject = StudentSubject.last
+
+    @test_update_student = Student.new
+    @test_update_student[:name] = "Update"
+    @test_update_student[:surname] = "Test-Student"
+    @test_update_student.save
+
+    @updated_studentSubject[:Student_id] = @test_update_student[:id]
+    @updated_studentSubject.save
+    assert_equal @post_add_len, StudentSubject.dataset.count
+  end
+
+  it "Checking if value is updated correctly" do
+    @updated_studentSubject = StudentSubject.last
+
+    @test_update_student = Student.new
+    @test_update_student[:name] = "Update"
+    @test_update_student[:surname] = "Test-Student"
+    @test_update_student.save
+
+    @updated_studentSubject[:Student_id] = @test_update_student[:id]
+    @updated_studentSubject.save
+    assert_equal @updated_studentSubject, StudentSubject.last
+  end
+
+  it "Deleting value should decrease dataset length" do
+    # skip("no mocks implemented")
+    @removed_studentSubject = StudentSubject.last
+    @removed_studentSubject.delete
+    assert_equal @post_add_len-1, StudentSubject.dataset.count
+  end
+
+  it "Checking if value is removed correctly" do
+    # skip("no mocks implemented")
+    @removed_studentSubject = StudentSubject.last
+    @removed_studentSubject.delete
+    refute_equal @removed_studentSubject, StudentSubject.last
+  end
+
+  it "Associating studentSubject to grade and removing studentSubject should remove grade" do
+
+    @removed_studentSubject = StudentSubject.last
+
+    @grade = Grade.new
+    @grade[:grade] = 5
+    @grade[:comment] = "Za projekt z ruby"
+    @grade[:date] = "08.01.2019"
+    @grade[:StudentSubject_id] = @removed_studentSubject[:id]
+    @grade.save
+
+    @removed_studentSubject.delete
+
+    Grade.last.wont_equal @grade
+  end
+
+  it "Checking print function" do
+    assert_output(/"2 |Testing subject |Test Teacher\n"/) {@studentSubject.printline(1)}
+  end
+
+  it "Checking print label function" do
+    assert_output {StudentSubject.printlabels}
+  end
+
+
+  after{
+    @test_teacher = nil
+    @test_subject = nil
+    @test_student = nil
+    @test_update_student = nil
+    @studentSubject = nil
+  }
+end
+
+describe "Negative cases - StudentSubject" do
+
+  before() do
+    Database.init
+    @studentSubject = StudentSubject.new
+  end
+
+  it "Assigning id of non existing Student" do
+    @studentSubject[:Student_id] = 1
+    assert_raises (Sequel::ValidationFailed) {@studentSubject.save}
+  end
+
+  it "Assigning id of non existing Subject" do
+    @studentSubject[:Subject_id] = 1
+    assert_raises (Sequel::ValidationFailed) {@studentSubject.save}
+  end
+
+  after() do
+    @studentSubject = nil
+  end
+
+
+end
+
+describe "Checking StudentSubject Class validation" do
+
+  before {
+    Database.init
+    @test_student = Student.new
+    @test_student[:name] = "Test"
+    @test_student[:surname] = "Student"
+    @test_student.save
+
+    @test_teacher = Teacher.new
+    @test_teacher[:name] = "Test"
+    @test_teacher[:surname] = "Teacher"
+    @test_teacher.save
+
+    @test_subject = Subject.new
+    @test_subject[:name] = "Test"
+    @test_subject[:Teacher_id] = @test_teacher[:id]
+    @test_subject.save
+
+    @studentSubject = StudentSubject.new
+    @studentSubject[:Student_id] = @test_student[:id]
+    @studentSubject[:Subject_id] = @test_subject[:id]
+
+  }
+
+
+  it "Checking error when student_id is nil" do
+    @studentSubject[:Student_id] = nil
+    assert_raises (Sequel::ValidationFailed) {@studentSubject.save}
+  end
+
+  it "Checking error when student_id is not integer" do
+    @studentSubject[:Student_id] = "test"
+    assert_raises (Sequel::ValidationFailed) {@studentSubject.save}
+  end
+
+  it "Checking error when student_id is negative integer" do
+    @studentSubject[:Student_id] = -1
+    assert_raises (Sequel::ValidationFailed) {@studentSubject.save}
+  end
+
+
+  it "Checking error when subject_id is nil" do
+    @studentSubject[:Subject_id] = nil
+    assert_raises (Sequel::ValidationFailed) {@studentSubject.save}
+  end
+
+  it "Checking error when subject_id is not integer" do
+    @studentSubject[:Subject_id] = "test"
+    assert_raises (Sequel::ValidationFailed) {@studentSubject.save}
+  end
+
+  it "Checking error when subject_id is negative integer" do
+    @studentSubject[:Subject_id] = -1
+    assert_raises (Sequel::ValidationFailed) {@studentSubject.save}
+  end
+
+  it "Checking error when subject_id and student_id is not unique pair" do
+    @studentSubject_test = StudentSubject.new
+    @studentSubject_test[:Student_id] = @test_student[:id]
+    @studentSubject_test[:Subject_id] = @test_subject[:id]
+    @studentSubject_test.save
+    assert_raises (Sequel::ValidationFailed) {@studentSubject.save}
+  end
+
+  after {
+    @studentSubject = nil
+    @test_teacher = nil
+    @test_student = nil
+    @test_subject = nil
+  }
+
 end
