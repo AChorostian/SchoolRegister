46fa72826509983b3925e10a724c1996f9fac454
inql <dbinkus96@gmail.com>
2018-12-22 18:54:45 +0100

    implemented minitest for subject

--- test/Student_test.rb
+++ test/Student_test.rb
@@ -22,86 +22,104 @@ describe "Checking Student class initialization" do
   }
 end
 
-describe "Checking Subject class functionality" do
+describe "Checking Student class functionality" do
 
   before do
     Database.init
+    @new_student = Student.new
+    @pre_add_len = Student.dataset.count
+    @new_student[:name] = "Jan"
+    @new_student[:surname] = "Kowalski"
+    @new_student.save
+    @post_add_len = Student.dataset.count
+  end
+
+
+  it "Inserting value should increase dataset length" do
+    assert_equal @pre_add_len+1, @post_add_len
+  end
+
+  it "Checking if value is inserted correctly" do
+    assert_equal @new_student, Student.last
+  end
+
+  it "Updating value shouldn't increase dataset length" do
+    @updated_student = Student.last
+    @updated_student[:surname] = "New-Surname"
+    @updated_student.save
+    assert_equal @post_add_len, Student.dataset.count
+  end
+
+  it "Checking if value is updated correctly" do
+    @updated_student = Student.last
+    @updated_student[:surname] = "New-Surname"
+    @updated_student.save
+    assert_equal @updated_student, Student.last
+  end
+
+  it "Deleting value should decrease dataset length" do
+    @removed_student = Student.last
+    @removed_student.delete
+    assert_equal @post_add_len-1, Student.dataset.count
+  end
+
+  it "Checking if value is removed correctly" do
+    # skip "need to check that later"
+    @removed_student = Student.last
+    @removed_student.delete
+    refute_equal @removed_student, Student.last
+  end
+
+  it "Associating student to note and removing student should remove note" do
+
     @test_teacher = Teacher.new
     @test_teacher[:name] = "Test"
     @test_teacher[:surname] = "Teacher"
     @test_teacher.save
 
-    @pre_add_len = Subject.dataset.count
+    @removed_student = Student.last
 
-    @subject = Subject.new
-    @subject[:name] = "Testing subjects"
-    @subject[:Teacher_id] = @test_teacher[:id]
-    @subject.save
+    @note = Note.new
+    @note[:description] = "Description of the note"
+    @note[:date] = "01.01.2018"
+    @note[:Student_id] = @removed_student[:id]
+    @note[:Teacher_id] = @test_teacher[:id]
+    @note.save
 
-    @post_add_len = Subject.dataset.count
+    @removed_student.delete
+
+    Note.last.wont_equal @note
   end
 
+  it "Associating student to StudentSubject and removing student should remove StudentSubject" do
+    @test_teacher = Teacher.new
+    @test_teacher[:name] = "Test"
+    @test_teacher[:surname] = "Teacher"
+    @test_teacher.save
 
-  it "Inserting value should increase dataset length" do
-    assert_equal @pre_add_len+1, Subject.dataset.count
-  end
+    @test_subject = Subject.new
+    @test_subject[:name] = "Test"
+    @test_subject[:Teacher_id] = @test_teacher[:id]
+    @test_subject.save
 
-  it "Checking if value is inserted correctly" do
-    assert_equal @subject, Subject.last
-  end
-
-  it "Updating value shouldn't increase dataset length" do
-    @updated_subject = Subject.last
-    @updated_subject[:name] = "Testing if subject is ok"
-    @updated_subject.save
-    assert_equal @post_add_len, Subject.dataset.count
-  end
-
-  it "Checking if value is updated correctly" do
-    @updated_subject = Subject.last
-    @updated_subject[:name] = "Testing if subject is ok"
-    @updated_subject.save
-    assert_equal @updated_subject, Subject.last
-  end
-
-  it "Deleting value should decrease dataset length" do
-    @removed_subject = Subject.last
-    @removed_subject.delete
-    assert_equal @post_add_len-1, Subject.dataset.count
-  end
-
-  it "Checking if value is removed correctly" do
-    # skip "need to check that later"
-    @removed_subject = Subject.last
-    @removed_subject.delete
-    refute_equal @removed_subject, Subject.last
-  end
-
-  it "Associating subject to StudentSubject and removing subject should remove StudentSubject" do
-
-    @test_student = Student.new
-    @test_student[:name] = "Test"
-    @test_student[:surname] = "Teacher"
-    @test_student.save
-
-    @removed_subject = Subject.last
+    @removed_student = Student.last
 
     @studentSubject = StudentSubject.new
-    @studentSubject[:Student_id] = @test_student[:id]
-    @studentSubject[:Subject_id] = @removed_subject[:id]
+    @studentSubject[:Student_id] = @removed_student[:id]
+    @studentSubject[:Subject_id] = @test_subject[:id]
     @studentSubject.save
 
-    @removed_subject.delete
+    @removed_student.delete
 
     StudentSubject.last.wont_equal @studentSubject
   end
 
   it "Checking print function" do
-    assert_output(/"2 |Testing subjects |Test Teacher\n"/) {@subject.printline(1)}
+    assert_output(/"2 |Jan |Kowalski\n"/) {@new_student.printline(1)}
   end
 
   it "Checking print label function" do
-    assert_output {Subject.printlabels}
+    assert_output {Student.printlabels}
   end
 
   after do
--- test/Subject_test.rb
+++ test/Subject_test.rb
@@ -21,3 +21,169 @@ describe "Checking Subject class initialization" do
     @new_subject = nil
   }
 end
+describe "Checking Subject class functionality" do
+
+  before do
+    Database.init
+    @test_teacher = Teacher.new
+    @test_teacher[:name] = "Test"
+    @test_teacher[:surname] = "Teacher"
+    @test_teacher.save
+
+    @pre_add_len = Subject.dataset.count
+
+    @subject = Subject.new
+    @subject[:name] = "Testing subjects"
+    @subject[:Teacher_id] = @test_teacher[:id]
+    @subject.save
+
+    @post_add_len = Subject.dataset.count
+  end
+
+
+  it "Inserting value should increase dataset length" do
+    assert_equal @pre_add_len+1, Subject.dataset.count
+  end
+
+  it "Checking if value is inserted correctly" do
+    assert_equal @subject, Subject.last
+  end
+
+  it "Updating value shouldn't increase dataset length" do
+    @updated_subject = Subject.last
+    @updated_subject[:name] = "Testing if subject is ok"
+    @updated_subject.save
+    assert_equal @post_add_len, Subject.dataset.count
+  end
+
+  it "Checking if value is updated correctly" do
+    @updated_subject = Subject.last
+    @updated_subject[:name] = "Testing if subject is ok"
+    @updated_subject.save
+    assert_equal @updated_subject, Subject.last
+  end
+
+  it "Deleting value should decrease dataset length" do
+    @removed_subject = Subject.last
+    @removed_subject.delete
+    assert_equal @post_add_len-1, Subject.dataset.count
+  end
+
+  it "Checking if value is removed correctly" do
+    # skip "need to check that later"
+    @removed_subject = Subject.last
+    @removed_subject.delete
+    refute_equal @removed_subject, Subject.last
+  end
+
+  it "Associating subject to StudentSubject and removing subject should remove StudentSubject" do
+
+    @test_student = Student.new
+    @test_student[:name] = "Test"
+    @test_student[:surname] = "Teacher"
+    @test_student.save
+
+    @removed_subject = Subject.last
+
+    @studentSubject = StudentSubject.new
+    @studentSubject[:Student_id] = @test_student[:id]
+    @studentSubject[:Subject_id] = @removed_subject[:id]
+    @studentSubject.save
+
+    @removed_subject.delete
+
+    StudentSubject.last.wont_equal @studentSubject
+  end
+
+  it "Checking print function" do
+    assert_output(/"2 |Testing subjects |Test Teacher\n"/) {@subject.printline(1)}
+  end
+
+  it "Checking print label function" do
+    assert_output {Subject.printlabels}
+  end
+
+  after do
+    @test_teacher = nil
+    @subject = nil
+  end
+end
+
+describe "Negative cases" do
+  before{
+    Database.init
+    @subject = Subject.new
+    @subject[:name] = "Testing subjects"
+  }
+
+  it "Assigning id of non existing teacher" do
+    @subject[:Teacher_id] = 1
+    assert_raises (Sequel::ValidationFailed) {@subject.save}
+  end
+
+  after{
+    @subject = nil
+  }
+end
+
+describe "Checking Subject class validation" do
+
+  before {
+    Database.init
+    @test_teacher = Teacher.new
+    @test_teacher[:name] = "Test"
+    @test_teacher[:surname] = "Teacher"
+    @test_teacher.save
+
+    @subject = Subject.new
+    @subject[:Teacher_id] = @test_teacher[:id]
+    @subject[:name] = "Test"
+  }
+
+  it "Checking error when name is nil" do
+    @subject[:name] = nil
+    assert_raises (Sequel::ValidationFailed) {@subject.save}
+  end
+
+  it "Checking error when name is too short" do
+    @subject[:name] = "T"
+    assert_raises (Sequel::ValidationFailed) {@subject.save}
+  end
+
+  it "Checking error when name is too long" do
+    @subject[:name] = "Test too long too long too long too long too long too long too long too long"
+    assert_raises (Sequel::ValidationFailed) {@subject.save}
+  end
+
+  it "Checking error when name is not String" do
+    @subject[:name] = [:name => "Test"]
+    assert_raises (Sequel::ValidationFailed) {@subject.save}
+  end
+
+  it "Checking error when name is in invalid format" do
+    @subject[:name] = "TEsting SUBJECT"
+    assert_raises (Sequel::ValidationFailed) {@subject.save}
+  end
+
+  it "Checking error when teacher_id is nil" do
+    @subject[:Teacher_id] = nil
+    assert_raises (Sequel::ValidationFailed) {@subject.save}
+  end
+
+  it "Checking error when teacher_id is not integer" do
+    @subject[:Teacher_id] = "Test"
+    assert_raises (Sequel::ValidationFailed) {@subject.save}
+  end
+
+  it "Checking error when teacher_id is negative integer" do
+    @subject[:Teacher_id] = -1
+    assert_raises (Sequel::ValidationFailed) {@subject.save}
+  end
+
+  after {
+
+    @subject = nil
+    @test_teacher = nil
+  }
+
+end
