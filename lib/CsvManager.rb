870b9cd9fbaa4e9503fcbe502b5b0e46126c67e0
Mateusz Małecki <mmateusz314@gmail.com>
2019-01-05 10:16:52 +0100

    Negative cases for csv files

--- lib/CsvManager.rb
+++ lib/CsvManager.rb
@@ -12,7 +12,7 @@ class CsvManager
         data.each do |elem|
           elem.delete(:id)
         end
-
+        begin
         CSV.open("data/"+filename,"w", encoding: "UTF-8",
                        :write_headers => true,
                        :headers => data.first.keys
@@ -21,20 +21,30 @@ class CsvManager
               csv << elem
             end
         end
+        rescue
+            raise ArgumentError, "Wrong filepath"
+        end
     end
 
     def self.importFromCsv(filepath,className)
 
         options = { encoding:"UTF-8", headers: true, col_sep: ','}
 
+        if (filepath[filepath.length-3,filepath.length] != "csv")
+            raise ArgumentError, "Wrong file type"
+        end
+
         if className.is_a? String
             className = Module.const_get(className)
         end
-
+        begin
         CSV.read(filepath, options).map do |row|
             h = row.to_hash
             h.delete("id")
             className.create(h)
+        end
+        rescue
+            raise ArgumentError, "Wrong filepath"
         end
 
     end
